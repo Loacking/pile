1989332b948543a8cfdac20ea0bafdf85ac9b0db
Loacking <LudovicDop@gmail.com>
2022-07-09 16:48:45 +0200

    Debut du prog de pile

--- pile/pile/main.c
+++ pile/pile/main.c
@@ -6,9 +6,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "pile.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    Pile *maPile = initialiser();
+    
+    empiler(maPile, 4);
+    empiler(maPile, 8);
+    empiler(maPile, 15);
+    empiler(maPile, 16);
+    empiler(maPile, 23);
+    empiler(maPile, 42);
+
+    printf("\nEtat de la pile :\n");
+    afficherPile(maPile);
+
+    printf("Je depile %d\n", depiler(maPile));
+    printf("Je depile %d\n", depiler(maPile));
+
+    printf("\nEtat de la pile :\n");
+    afficherPile(maPile);
     return 0;
 }
---
+++ pile/pile/pile.h
@@ -0,0 +1,60 @@
+//
+//  pile.h
+//  pile
+//
+//  Created by Ludovic Doppler on 09/07/2022.
+//
+
+#ifndef pile_h
+#define pile_h
+
+/* Mes structures */
+typedef struct Element Element;
+struct Element{
+    int nombre;
+    Element *suivant;
+};
+typedef struct Pile Pile;
+struct Pile{
+    Element *premier;
+};
+/* Mes fonctions */
+void empiler(Pile *pile,int nvNombre){
+    Element *nouveau = malloc(sizeof(nouveau));
+    if(pile == NULL || nouveau == NULL){
+        exit(EXIT_FAILURE);
+    }
+    
+    nouveau->nombre = nvNombre;
+    nouveau->suivant = pile->premier;
+    pile->premier = nouveau;
+    
+}
+int depiler(Pile *pile){
+    if(pile == NULL){
+        exit(EXIT_FAILURE);
+    }
+    int nombreDepile = 0;
+    Element *elementDepile = pile->premier;
+    
+    if(pile != NULL && pile->premier != NULL){
+        nombreDepile = elementDepile->nombre;
+        pile->premier = elementDepile->suivant;
+        free(elementDepile);
+    }
+    return nombreDepile;
+}
+
+void afficherPile(Pile *pile){
+    if(pile == NULL){
+        exit(EXIT_FAILURE);
+    }
+    Element *actuel = pile->premier;
+    
+    while (actuel != NULL) {
+        printf("%d\n", actuel->nombre);
+        actuel = actuel->suivant;
+    }
+    printf("\n");
+}
+#endif /* pile_h */
